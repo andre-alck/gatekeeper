746429986d3720f38e89284cbdddbe5ac9e8dae4
andre-alck <andrealck1@gmail.com>
2023-11-12 00:35:14 -0300

    feat: init login page

---
+++ src/main/java/br/com/asac/gatekeeper/login/controller/LoginMB.java
@@ -0,0 +1,77 @@
+package br.com.asac.gatekeeper.login.controller;
+
+import java.io.Serializable;
+
+import br.com.asac.gatekeeper.user.model.User;
+import br.com.asac.gatekeeper.user.service.UserService;
+import br.com.asac.gatekeeper.utils.controller.UIComponentUtils;
+import br.com.asac.gatekeeper.utils.controller.primefaces.UiComponentUtilsPrimeFaces;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Named;
+
+@Named
+@RequestScoped
+public class LoginMB implements Serializable {
+
+	private static final long serialVersionUID = -5915798641142750981L;
+
+	private User user;
+	private UserService userService;
+	private UIComponentUtils uiComponentUtils;
+	private static final String COULDNT_IDENTIFY_USER_DIALOG_ID = "couldnt-identify-user";
+	public static final String SUCCESS_PAGE_ROUTE = "success.jsf";
+
+	public LoginMB() {
+		this.setUser(new User());
+		this.setUserService(new UserService());
+		this.setUiComponentUtils(new UiComponentUtilsPrimeFaces());
+	}
+
+	public LoginMB(User user, UserService userService, UIComponentUtils uiComponentUtils) {
+		this.setUser(user);
+		this.setUserService(userService);
+		this.setUiComponentUtils(uiComponentUtils);
+	}
+
+	public String login() {
+		if (this.getUserService().isUserRegistered(this.getUser())) {
+			// return LoginMB.SUCCESS_PAGE_ROUTE + "?faces-redirect=true";
+			return LoginMB.SUCCESS_PAGE_ROUTE;
+		}
+
+		this.getUiComponentUtils().showDialog(LoginMB.COULDNT_IDENTIFY_USER_DIALOG_ID);
+		return null;
+	}
+
+	public User getUser() {
+		return this.user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public UserService getUserService() {
+		return this.userService;
+	}
+
+	public void setUserService(UserService userService) {
+		this.userService = userService;
+	}
+
+	public UIComponentUtils getUiComponentUtils() {
+		return this.uiComponentUtils;
+	}
+
+	public void setUiComponentUtils(UIComponentUtils uiComponentUtils) {
+		this.uiComponentUtils = uiComponentUtils;
+	}
+
+	public static String getCouldntIdentifyUserDialogId() {
+		return LoginMB.COULDNT_IDENTIFY_USER_DIALOG_ID;
+	}
+
+	public static String getSuccessPageRoute() {
+		return LoginMB.SUCCESS_PAGE_ROUTE;
+	}
+}
---
+++ src/test/java/br/com/asac/gatekeeper/login/controller/LoginMBTest.java
@@ -0,0 +1,101 @@
+package br.com.asac.gatekeeper.login.controller;
+
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import br.com.asac.gatekeeper.user.service.UserService;
+import br.com.asac.gatekeeper.utils.controller.UIComponentUtils;
+
+public class LoginMBTest {
+	private UserService userService;
+	private UIComponentUtils uiComponentUtils;
+
+	@BeforeEach
+	void clearMocks() {
+		this.setUserService(null);
+		this.setUiComponentUtils(null);
+	}
+
+	@Test
+	void givenUnregisteredUser_whenLogin_thenShowDialogAndDoNotLogin() {
+		// arrange
+		this.setUserService(this.createUnregisteredUserUserServiceMock());
+		this.setUiComponentUtils(this.createMockedUIComponentUtils());
+		LoginMB loginMB = new LoginMB(null, this.getUserService(), this.getUiComponentUtils());
+
+		// act
+		String successPageRoute = loginMB.login();
+
+		// assert
+		assertNull(successPageRoute);
+		verify(this.getUserService(), times(1)).isUserRegistered(any());
+		verify(this.getUiComponentUtils(), times(1)).showDialog(LoginMB.getCouldntIdentifyUserDialogId());
+	}
+
+	@Test
+	void givenRegisteredUser_whenLogin_thenLoginAndDoNotShowDialog() {
+		// arrange
+		this.setUserService(this.createRegisteredUserUserServiceMock());
+		this.setUiComponentUtils(this.createMockedUIComponentUtils());
+		LoginMB loginMB = new LoginMB(null, this.getUserService(), this.getUiComponentUtils());
+
+		/// act
+		String successPageRoute = loginMB.login();
+
+		// assert
+		assertEquals(successPageRoute, LoginMB.SUCCESS_PAGE_ROUTE);
+		verify(this.getUserService(), times(1)).isUserRegistered(any());
+		verify(this.getUiComponentUtils(), times(0)).showDialog(LoginMB.getCouldntIdentifyUserDialogId());
+	}
+
+	private UserService createUnregisteredUserUserServiceMock() {
+		return this.createMockedUserService();
+	}
+
+	private UserService createRegisteredUserUserServiceMock() {
+		UserService userService = this.createMockedUserService();
+		when(userService.isUserRegistered(any())).thenReturn(true);
+		return userService;
+	}
+
+	private UserService createMockedUserService() {
+		UserService userService = mock(UserService.class);
+		assertTrue(Mockito.mockingDetails(userService).isMock());
+		return userService;
+	}
+
+	private UIComponentUtils createMockedUIComponentUtils() {
+		UIComponentUtils uiComponentUtils = mock(UIComponentUtils.class);
+		assertTrue(Mockito.mockingDetails(uiComponentUtils).isMock());
+		return uiComponentUtils;
+	}
+
+	/* getters n setters */
+
+	private UserService getUserService() {
+		return userService;
+	}
+
+	private void setUserService(UserService userService) {
+		this.userService = userService;
+	}
+
+	private UIComponentUtils getUiComponentUtils() {
+		return uiComponentUtils;
+	}
+
+	private void setUiComponentUtils(UIComponentUtils uiComponentUtils) {
+		this.uiComponentUtils = uiComponentUtils;
+	}
+}
